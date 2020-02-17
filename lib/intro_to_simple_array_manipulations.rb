57e3e2c0312e8eaa846aef49673a582e895bc554
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-17 18:59:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -53,7 +53,7 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  
+  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
 end
 
 def using_delete_at(array, integer)
