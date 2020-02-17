8b5c81cf066876d6921710903832033902f6d0c8
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-17 18:36:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,8 +12,5 @@ end
 
 def using_pop(array)
   @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  @continents.pop
+  @continents.pop()
 end
-
-def pop_with_args(array)
-  
