c2979fd10df4a4136dec0d0fec3298572c798fda
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-17 18:38:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,4 +21,6 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
-  
+  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  @my_favorite_cities.shift
+end
