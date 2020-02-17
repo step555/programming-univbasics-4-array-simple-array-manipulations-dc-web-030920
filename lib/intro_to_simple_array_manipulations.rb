53e4a566dd729655d42171b540469cc7ae01b8a2
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-17 18:35:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,3 +14,6 @@ def using_pop(array)
   @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   @continents.pop
 end
+
+def pop_with_args(array)
+  
