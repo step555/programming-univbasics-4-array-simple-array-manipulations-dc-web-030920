072c4a8584ff660a0c2928a4af8b1494d1be1425
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-17 19:03:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -54,7 +54,7 @@ end
 
 def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete("Steven")
+  instructors.delete("Steven")
 end
 
 # def using_delete_at(array, integer)
